fabbb1e5d8e0d6cc1d252fd6b00971ce2dbdff6c
André Rosa <andre.reddrain@gmail.com>
2020-04-21 19:05:03 +0100

    3.3 - Submitting Forms

--- src/Repository/GameRepository.php
+++ src/Repository/GameRepository.php
@@ -5,6 +5,34 @@ require __DIR__ . "/../Entity/Rating.php";
 
 class GameRepository
 {
+    protected $pdo;
+
+    public function __construct()
+    {
+        $dsn = "mysql:host=mysql;dbname=gamebook_test;";
+        $user = "root";
+        $pass = "root";
+
+        $this->pdo = new PDO($dsn, $user, $pass);
+    }
+
+    public function findById($id)
+    {
+        $statement = $this->pdo->prepare('SELECT * FROM game WHERE id = ?');
+        $statement->execute([$id]);
+        $game = $statement->fetchObject('Game');
+        return $game;
+    }
+
+    public function saveGameRating($gameId, $userId, $score)
+    {
+        $statement = $this->pdo->prepare(
+            'REPLACE INTO rating (game_id, user_id, score)
+            VALUES(?, ?, ?)'
+        );
+        return $statement->execute([$gameId, $userId, $score]);
+    }
+
     /**
      * Populate method.
      * 
--- src/Test/Controller/GameControllerTest.php
+++ src/Test/Controller/GameControllerTest.php
@@ -5,11 +5,60 @@ use Goutte\Client;
 
 class GameControllerTest extends TestCase
 {
+    protected $pdo;
+
     public function testIndex_HasTitle()
     {
         $client = new Client();
         $response = $client->request('GET', 'http://localhost/Projetos/tdd_sample_project/web/');
         $this->assertCount(6, $response->filter('ul > li'));
-        // $this->assertRegExp('/<title>/', $response->getBody()->getContents());
+    }
+
+    public function testAddRating_WithGet_HasEmptyForm()
+    {
+        $client = new Client();
+        $response = $client->request('GET',
+            'http://localhost/Projetos/tdd_sample_project/web/add-rating.php?game=1'
+        );
+
+        $this->assertCount(1, $response->filter('form'));
+        $this->assertEquals('',
+            $response->filter('form input[name=score]')->attr('value')
+        );
+    }
+
+    public function testAddRating_WithPost_IsRedirect()
+    {
+        $client = new \GuzzleHttp\Client();
+        $response = $client->request('POST',
+            'http://localhost/Projetos/tdd_sample_project/web/add-rating.php?game=1',
+            [
+                'allow_redirects' => false,
+                'form_params' => [
+                    'score' => '5'
+                ],
+            ]
+        );
+        $this->assertEquals(302, $response->getStatusCode());
+        $this->assertEquals('/', $response->getHeaderLine('Location'));
+
+        $dsn = "mysql:host=127.0.0.1;dbname=gamebook_test;port=3306";
+        $user = "root";
+        $pass = "root";
+
+        $pdo = new PDO($dsn, $user, $pass);
+
+        $statement = $pdo->prepare('SELECT * FROM rating');
+        $statement->execute();
+        $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+        $this->assertCount(1, $result);
+        $this->assertEquals(
+            [
+                'user_id' => '1',
+                'game_id'=> '1',
+                'score' => '5',
+            ], $result[0]
+        );
     }
 }
