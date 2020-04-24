ee0ddd39eaf5c5c7f45890bfd54f287deb2fd347
André Rosa <andre.reddrain@gmail.com>
2020-04-24 16:11:32 +0100

    5.1 - Running Tests Automatically

--- src/Test/Unit/UserTest.php
+++ src/Test/Unit/UserTest.php
@@ -2,8 +2,6 @@
 
 use PHPUnit\Framework\TestCase;
 
-require __DIR__ . "/../../Entity/User.php";
-
 class UserTest extends TestCase
 {
     public function testGenreCompatibility_With8And6_Returns7()
---
+++ src/Test/bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+
+require __DIR__ . "/../../vendor/autoload.php";
+
+require __DIR__ . "/../Entity/Game.php";
+require __DIR__ . "/../Entity/User.php";
+require __DIR__ . "/../Entity/Rating.php";
