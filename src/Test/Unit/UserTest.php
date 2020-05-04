ce5d1e70836d874285215cbe5a3cad54388baac6
André Rosa <andre.reddrain@gmail.com>
2020-05-04 17:18:46 +0100

    Fix on UserTest Mock Syntax.

--- src/Test/Unit/UserTest.php
+++ src/Test/Unit/UserTest.php
@@ -6,46 +6,59 @@ class UserTest extends TestCase
 {
     public function testGenreCompatibility_With8And6_Returns7()
     {
-        $rating1 = $this->createMock('Rating', ['getScore']);
-        $rating1->method('getScore')
-                ->willReturn(6);
+        $rating1 = $this->getMockBuilder(Rating::class)
+            ->setMethods(['getScore'])
+            ->getMock();
+        $rating1->method('getScore')->willReturn(6);
 
-        $rating2 = $this->createMock('Rating', ['getScore']);
-        $rating2->method('getScore')
-                ->willReturn(8);
-        
-        $user = $this->createMock('User', ['findRatingsByGenre']);
-        $user->method('findRatingsByGenre')
-             ->willReturn([$rating1, $rating2]);
-        $this->assertEquals(7, $user->getGenreCompatibility('zombies'));
+        $rating2 = $this->getMockBuilder(Rating::class)
+            ->setMethods(['getScore'])
+            ->getMock();
+        $rating2->method('getScore')->willReturn(8);
+
+        $user = $this->getMockBuilder(User::class)
+            ->setMethods(['findRatingsByGenre'])
+            ->getMock();
+        $user->method('findRatingsByGenre')->willReturn([$rating1, $rating2]);
+
+        $result = $user->getGenreCompatibility('zombies');
+
+        $this->assertEquals(7, $result);
     }
 
     public function testRatingsByGenre_With1ZombieAnd1Shooter_Returns1Zombie()
     {
-        $zombiesGame = $this->createMock('Game', ['getGenreCode']);
-        $zombiesGame->method('getGenreCode')
-                    ->willReturn('zombies');
+        $zombiesGame = $this->getMockBuilder(Game::class)
+            ->setMethods(['getGenreCode'])
+            ->getMock();
+        $zombiesGame->method('getGenreCode')->willReturn('zombies');
 
-        $shooterGame = $this->createMock('Game', ['getGenreCode']);
-        $shooterGame->method('getGenreCode')
-                    ->willReturn('shooter');
+        $shooterGame = $this->getMockBuilder(Game::class)
+            ->setMethods(['getGenreCode'])
+            ->getMock();
+        $shooterGame->method('getGenreCode')->willReturn('shooter');
 
-        $rating1 = $this->createMock('Rating', ['getGame']);
-        $rating1->method('getGame')
-                ->willReturn($zombiesGame);
+        $rating1 = $this->getMockBuilder(Rating::class)
+            ->setMethods(['getScore'])
+            ->getMock();
+        $rating1->method('getScore')->willReturn($zombiesGame);
 
-        $rating2 = $this->createMock('Rating', ['getGame']);
-        $rating2->method('getGame')
-                ->willReturn($shooterGame);
-        
-        $user = $this->createMock('User', ['getRatings']);
-        $user->method('getRatings')
-             ->willReturn([$rating1, $rating2]);
+        $rating2 = $this->getMockBuilder(Rating::class)
+            ->setMethods(['getScore'])
+            ->getMock();
+        $rating2->method('getScore')->willReturn($shooterGame);
+
+        $user = $this->getMockBuilder(User::class)
+            ->setMethods(['findRatingsByGenre'])
+            ->getMock();
+        $user->method('findRatingsByGenre')->willReturn([$rating1, $rating2]);
 
         $ratings = $user->findRatingsByGenre('zombies');
         $this->assertCount(1, $ratings);
+        
         foreach ($ratings as $rating) {
-            $this->assertEquals('zombies', $rating->getGame()->getGenreCode());
+            $result = $rating->getGame()->getGenreCode();
+            $this->assertEquals('zombies', $result);
         }
     }
 }
